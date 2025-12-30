86d780833d4b0aaef83850d4066c67589014ab49
Omonigho Victoria <omonighooghenetega16@gmail.com>
2025-12-30 05:23:24 +0100

    Blog feat: Sanity setup done

---
+++ sanity/sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Siru Design Blog',
+
+  projectId: 'jqmr254x',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
